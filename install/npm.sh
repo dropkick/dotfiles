914687bad40a90144b2360d2acd01ca520e7fa72
Jason Duerr <jason@dropkickdesign.com>
2018-01-25 01:24:21 -0800

    update to 8, update packages

--- install/npm.sh
+++ install/npm.sh
@@ -1,4 +1,3 @@
-
 if ! is-executable brew -o ! is-executable git; then
   echo "Skipped: npm (missing: brew and/or git)"
   return
@@ -10,20 +9,15 @@ export DOTFILES_BREW_PREFIX_NVM=`brew --prefix nvm`
 set-config "DOTFILES_BREW_PREFIX_NVM" "$DOTFILES_BREW_PREFIX_NVM" "$DOTFILES_CACHE"
 
 . "${DOTFILES_DIR}/system/.nvm"
-nvm install 6
-
-# nvm install 8
-# nvm use 8
-# nvm alias default 8
+nvm install 8
+nvm alias default 8
 
 # Globally install with npm
 
 packages=(
-  diff-so-fancy
-  grunt
   gtop
   gulp
-  http-server
+  historie
   nodemon
   npm
   release-it
