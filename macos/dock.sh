1006f923b5b2ef7ebbc8a862c44b926fdd306984
Jason Duerr <jason@dropkickdesign.com>
2018-01-25 02:01:33 -0800

    app changes

--- macos/dock.sh
+++ macos/dock.sh
@@ -7,13 +7,12 @@ dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/iTunes.app"
 dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Messages.app"
-dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Tweetbot.app"
 dockutil --no-restart --add "/Applications/Reeder.app"
 dockutil --no-restart --add "/Applications/Pages.app"
 dockutil --no-restart --add "/Applications/Numbers.app"
 dockutil --no-restart --add "/Applications/PDFScanner.app"
-dockutil --no-restart --add "/Applications/Evernote.app"
+dockutil --no-restart --add "/Applications/DEVONthink Pro Office.app"
 dockutil --no-restart --add "/Applications/Quiver.app"
 dockutil --no-restart --add "/Applications/rubiTrack 4 Pro.app"
 # dockutil --no-restart --add '' --type spacer --section apps
@@ -29,11 +28,10 @@ dockutil --no-restart --add "/Applications/TorBrowser.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Sublime Text 2.app"
 dockutil --no-restart --add "/Applications/Tower.app"
-dockutil --no-restart --add "/Applications/CodeKit.app"
 dockutil --no-restart --add "/Applications/Dash.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Utilities/Console.app"
-dockutil --add '~/Projects' --view list --display folder
 dockutil --add '/Applications' --view list --display folder
+dockutil --add '~/Projects' --view list --display folder
 dockutil --add '~/Downloads' --view list --display folder
 killall Dock
