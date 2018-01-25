03ce3267e8a4a7c777d96925c4eda7b2f5b14394
Jason Duerr <jason@dropkickdesign.com>
2018-01-25 01:16:49 -0800

    udpated packages, link mackup.cfg

--- install/brew.sh
+++ install/brew.sh
@@ -1,9 +1,12 @@
-\# Install Homebrew
+if ! is-macos -o ! is-executable ruby -o ! is-executable curl -o ! is-executable git; then
+  echo "Skipped: Homebrew (missing: ruby, curl and/or git)"
+  return
+fi
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap homebrew/versions
-brew tap homebrew/dupes
+# brew tap homebrew/dupes
 brew tap Goles/battery
 brew update
 brew upgrade
@@ -16,12 +19,13 @@ apps=(
   battery
   coreutils
   cmake
+  diff-so-fancy
   dockutil
-  docker
   ffmpeg
   fasd
   gifsicle
   git
+  git-extras
   gnu-sed --with-default-names
   grep --with-default-names
   hub
@@ -30,18 +34,25 @@ apps=(
   jq
   lynx
   mackup
-  homebrew/dupes/openssh
+  nano
+# may not need to update openssh on macos 
+#  homebrew/dupes/openssh
+  pandoc
   peco
   psgrep
   python
-  p7zip
   shellcheck
   ssh-copy-id
-  svn
   tree
+  unar
   vim
   wget
   wifi-password
 )
 
 brew install "${apps[@]}"
+
+export DOTFILES_BREW_PREFIX_COREUTILS=`brew --prefix coreutils`
+set-config "DOTFILES_BREW_PREFIX_COREUTILS" "$DOTFILES_BREW_PREFIX_COREUTILS" "$DOTFILES_CACHE"
+
+ln -sfv "$DOTFILES_DIR/etc/mackup/.mackup.cfg" ~
