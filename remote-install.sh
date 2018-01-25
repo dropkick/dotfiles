9bb091f997c7b54ea18c2e5960601e31ae595e5a
Jason Duerr <jason@dropkickdesign.com>
2018-01-25 02:58:58 -0800

    remote install update

--- remote-install.sh
+++ remote-install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-[[ -x `command -v wget` ]] && CMD="wget --no-check-certificate -O -"
-[[ -x `command -v curl` ]] >/dev/null 2>&1 && CMD="curl -#L"
+if [ -x "`command -v curl`" ]; then
+  CMD="curl -#L"
+elif [ -x "`command -v wget`" ]; then
+  CMD="wget --no-check-certificate -O -"
+fi
 
 if [ -z "$CMD" ]; then
   echo "No curl or wget available. Aborting."
@@ -11,3 +14,4 @@ else
   eval "$CMD https://github.com/dropkick/dotfiles/tarball/master | tar -xzv -C ~/.dotfiles --strip-components=1 --exclude='{.gitignore}'"
   . "$HOME/.dotfiles/install.sh"
 fi
+
