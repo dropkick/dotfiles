c129bf79a78133dbd688fe8c7c642fdf26222511
Jason Duerr <jason@dropkickdesign.com>
2018-05-15 23:15:29 -0700

    Fix up tab completion

--- install/gem.sh
+++ install/gem.sh
@@ -7,8 +7,8 @@ brew install gpg2
 
 \curl -sSL https://get.rvm.io | bash -s stable
 
-rvm install 2.3
-rvm use 2.3 --default
+rvm install 2.5
+rvm use 2.5 --default
 
 gem install lunchy
 gem install pygmentize
