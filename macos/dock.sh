1424243d61d5131a86562e45395dd4ad675f8d08
Jason Duerr <jason@dropkickdesign.com>
2018-11-02 12:13:04 -0700

    Move browsers into their own dock stack
    Signed-off-by: Jason Duerr <jason@dropkickdesign.com>

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,36 +1,38 @@
 #!/bin/sh
 
+# Set up a 'browsers' folder with symlinks to all browsers to use as a launcher in dock
+mkdir -p $HOME/Stacks/browsers
+
+# Link browsers
+ln -s '/Applications/Firefox.app' ~/Stacks/browsers/1\ -\ Firefox
+ln -s '/Applications/Firefox Developer Edition.app' ~/Stacks/browsers/2\ -\ Firefox\ Developer
+ln -s '/Applications/Google Chrome.app' ~/Stacks/browsers/3\ -\ Chrome
+ln -s '/Applications/Google Chrome Canary.app' ~/Stacks/browsers/4\ -\ Chrome\ Canary
+ln -s '/Applications/Safari.app' ~/Stacks/browsers/5\ -\ Safari
+ln -s '/Applications/Safari Technology Preview.app' ~/Stacks/browsers/6\ -\ Safari\ Preview
+ln -s '/Applications/Opera.app' ~/Stacks/browsers/7\ -\ Opera
+ln -s '/Applications/Epic.app' ~/Stacks/browsers/8\ -\ Epic
+ln -s '/Applications/TorBrowser.app' ~/Stacks/browsers/9\ -\ TorBrowser
+ln -s '/Applications/Reeder.app' ~/Stacks/browsers/10\ -\ Reeder
+
 # Position (left, bottom, right) 
 defaults write com.apple.dock orientation -string "left" 
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/iTunes.app"
-dockutil --no-restart --add "/Applications/Mail.app"
-dockutil --no-restart --add "/Applications/Messages.app"
-dockutil --no-restart --add "/Applications/Tweetbot.app"
-dockutil --no-restart --add "/Applications/Reeder.app"
-dockutil --no-restart --add "/Applications/Pages.app"
-dockutil --no-restart --add "/Applications/Numbers.app"
-dockutil --no-restart --add "/Applications/PDFScanner.app"
-dockutil --no-restart --add "/Applications/DEVONthink Pro Office.app"
-dockutil --no-restart --add "/Applications/Quiver.app"
-dockutil --no-restart --add "/Applications/rubiTrack 4 Pro.app"
-# dockutil --no-restart --add '' --type spacer --section apps
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Google Chrome Canary.app"
-dockutil --no-restart --add "/Applications/Firefox.app"
-dockutil --no-restart --add "/Applications/FirefoxDeveloperEdition.app"
-dockutil --no-restart --add "/Applications/Safari.app"
-dockutil --no-restart --add "/Applications/Safari Technology Preview.app"
-dockutil --no-restart --add "/Applications/Opera.app"
-dockutil --no-restart --add "/Applications/Epic.app"
-dockutil --no-restart --add "/Applications/TorBrowser.app"
+dockutil --no-restart --add "/Applications/MailMate.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
-dockutil --no-restart --add "/Applications/Sublime Text 2.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Tower.app"
 dockutil --no-restart --add "/Applications/Dash.app"
-dockutil --no-restart --add "/Applications/System Preferences.app"
-dockutil --no-restart --add "/Applications/Utilities/Console.app"
+# dockutil --no-restart --add '' --type spacer --section apps
+dockutil --no-restart --add "/Applications/nvALT.app"
+dockutil --no-restart --add "/Applications/DEVONthink Pro Office.app"
+dockutil --no-restart --add "/Applications/PDFScanner.app"
+dockutil --no-restart --add '' --type spacer --section apps
+dockutil --add '~/Stacks/browsers' --view list --display stack --sort name
+dockutil --add 'vnc://dynamo.dropkick.us' --label 'dynamo.dropkick.us VNC'
+dockutil --add 'afp://dynamo.dropkick.us' --label 'dynamo.dropkick.us AFP'
+dockutil --add 'afp://projects.sandstrompartners.com' --label 'projects.sandstrom AFP'
 dockutil --add '/Applications' --view list --display folder
 dockutil --add '~/Projects' --view list --display folder
 dockutil --add '~/Downloads' --view list --display folder
