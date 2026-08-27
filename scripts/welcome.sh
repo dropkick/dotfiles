7ae05f0b763d4e344bab938746b29f8b5d6bf5b3
Jason Duerr <jason@dropkickdesign.com>
2026-08-27 01:42:37 -0700

    update welcome script

--- scripts/welcome.sh
+++ scripts/welcome.sh
@@ -23,7 +23,7 @@ rm -f /tmp/now-weather
 # zsh arrays are 1-indexed: aWeather[1] is the first line
 if [[ "${aWeather[1]}" == "Weather report:"* ]] ; then
     WeatherSuccess=true
-    echo "${aWeather[@]}"
+    print -l "${aWeather[@]}"
 else
     WeatherSuccess=false
     echo "+============================+"
@@ -39,11 +39,84 @@ echo " "                # Pad blank lines for calendar & time to fit
 
 #--------- DATE -------------------------------------------------------------
 
-# calendar current month with today highlighted.
-# colors 00=bright white, 31=red, 32=green, 33=yellow, 34=blue, 35=purple,
-#        36=cyan, 37=white
+tput sc                 # Save cursor position.
+
+# Move up 9 lines
+i=0
+while [ $((++i)) -lt 10 ]; do tput cuu1; done
+
+if [[ "$WeatherSuccess" == true ]] ; then
+    Column=$((DateColumn - 10))
+    tput cuf $Column        # Move x column number
+    printf "          "     # Blank out ", country" with spaces
+else
+    tput cuf $DateColumn    # Position to column 34 for date display
+fi
+
+# Generate calendar and strip control characters
+cal > /tmp/terminal1
+tr -cd '\11\12\15\40\60-\136\140-\176' < /tmp/terminal1 > /tmp/terminal
+
+CalLineCnt=1
+Today=$(date +"%e")
+
+printf "\033[32m"   # color green
+
+while IFS= read -r Cal; do
+    printf "%s" "$Cal"
+    if [[ $CalLineCnt -gt 2 ]] ; then
+        # See if today is on current line & invert background
+        tput cub 22
+        for (( j=0 ; j <= 18 ; j += 3 )) ; do
+            Test=${Cal:$j:2}            # Current day on calendar line
+            if [[ "$Test" == "$Today" ]] ; then
+                printf "\033[7m"        # Reverse: [ 7 m
+                printf "%s" "$Today"
+                printf "\033[0m"        # Normal: [ 0 m
+                printf "\033[32m"       # color green
+                tput cuf 1
+            else
+                tput cuf 3
+            fi
+        done
+    fi
+
+    tput cud1               # Down one line
+    tput cuf $DateColumn    # Move right
+    CalLineCnt=$((++CalLineCnt))
+done < /tmp/terminal
+
+printf "\033[00m"           # color -- bright white (default)
+echo ""
+
+tput rc                     # Restore saved cursor position.
+
+#-------- TIME --------------------------------------------------------------
 
 tput sc                 # Save cursor position.
-# Move up 9 lines
-# Strip control characters from cal output
-tr -cd '\11\12\15\40\60-\136\140-\176' < /tmp/terminal1 > /tmp/terminal
+
+# Move up 8 lines
+i=0
+while [ $((++i)) -lt 9 ]; do tput cuu1; done
+tput cuf $TimeColumn    # Move 61 columns right
+
+# Do we have the toilet package?
+if command -v toilet &>/dev/null; then
+    echo " $(date +"%H:%M") " | \
+        toilet -f future --filter border > /tmp/terminal
+# Do we have the figlet package?
+elif command -v figlet &>/dev/null; then
+    date +"%H:%M" | figlet > /tmp/terminal
+# else use standard font
+else
+    date +"%H:%M" > /tmp/terminal
+fi
+
+while IFS= read -r Time; do
+    printf "\033[01;32m"    # color green
+    printf "%s" "$Time"
+    tput cud1               # Up one line
+    tput cuf $TimeColumn    # Move right
+done < /tmp/terminal
+
+tput rc                     # Restore saved cursor position.
